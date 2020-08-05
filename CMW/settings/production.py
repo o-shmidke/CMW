6e8d3797da3eecf510ede3159cae0d5429d976bf
o-shmidke <61247061+o-shmidke@users.noreply.github.com>
2020-08-05 23:36:07 +0500

    !

--- CMW/settings/production.py
+++ CMW/settings/production.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cmw-app.herokuapp.com']
+ALLOWED_HOSTS = ['cmw-app.herokuapp.com/']
 
 # Application definition
 
