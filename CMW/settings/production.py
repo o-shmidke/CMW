ee86f840a31905471974d053e259100d03468839
o-shmidke <61247061+o-shmidke@users.noreply.github.com>
2020-08-12 22:29:32 +0500

    !

--- CMW/settings/production.py
+++ CMW/settings/production.py
@@ -133,6 +133,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, 'media'),
 ]
 MEDIA_URL = '/media/'
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
