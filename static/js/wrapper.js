101db274b677b6d1563d964ccc8952411438533c
o-shmidke <61247061+o-shmidke@users.noreply.github.com>
2020-08-18 18:17:28 +0500

    !

--- static/js/wrapper.js
+++ static/js/wrapper.js
@@ -5,65 +5,23 @@ $('#icon_logo').mouseenter(function () {
     $('#logo_move').removeClass('fa-spin')
 });
 
+// ----------------------------------------------открытие карточки пользователя
+$("#navbarDropdown").click(function (e) {
+        let container = $("#gb_F ");
+        if (container.css("display") === "block") {
+            container.slideUp(400)
+        } else {
+            container.slideDown(400);
+        }
 
-// ----------------------------------------открыть карточку пользователя
-// function showHide() {
-//     $('#gb_F')
-//     //Если элемент с id-шником element_id существует
-//     if (document.getElementById("gb_F")) {
-//         //Записываем ссылку на элемент в переменную obj
-//         var obj = document.getElementById("gb_F");
-//         //Если css-свойство display не block, то:
-//         if (obj.style.display === "block") {
-//
-//             obj.style.display = "none"; //Показываем элемент
-//         } else obj.style.display = "block"; //Скрываем элемент
-//     }
-//     //Если элемент с id-шником element_id не найден, то выводим сообщение
-//     else alert("Элемент с id: " + gb_F + " не найден!");
-// }
-
-// ---------------------------------------открытие формы для добавления фото
-// $('#upload_photo').click('mouseclick', function (e) {
-//
-//     $.post('/create_photo_form/', $(this).serialize(), function (data) {
-//         $('.form_photo').html(data)
-//     });
-//     e.preventDefault();
-// });
-
-
-//
-// function close_form(form_upload_photo) {
-//     var a = document.getElementById("form_upload_photo");
-//     a.style.display = "none";
-// }
-
-$(document).mouseup(function (e) {
-    var container = $("#gb_F ");
-    if (container.has(e.target).length === 0) {
-        container.hide();
+        e.stopPropagation();
     }
+);
+$('#gb_F').click(function (e) {
+    e.stopPropagation();
+});
+$(document).click(function () {
+    $('#gb_F').slideUp(400);
 });
 
-// $('document').ready(function () {
-//
-//     $('.navbarDropdown').click(function () {
-//           var container = $("#gb_F ");
-//     if (container.has(e.target).length === 0) {
-//         container.hide();
-//         container.visible
-//     }
-//
-//
-//     });
-//
-// });
-$( document ).ready(function(){
-	  // $( "#navbarDropdown" ).click(function(){ // задаем функцию при нажатиии на элемент с классом toggle
-	  //   $( "#gb_F" ).toggle( "fast" ); // скрывыаем, или отображаем последний элемент <div> в документе
-	  // });
-	  $( "#navbarDropdown" ).click(function(){ // задаем функцию при нажатиии на элемент с классом slide-toggle
-	    $( "#gb_F" ).slideToggle( "100" ); // скрывыаем, или отображаем последний элемент <div> в документе
-	  });
-	});
+
