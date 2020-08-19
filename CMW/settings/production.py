696c08ebe52f3fe8c16404103306184d747158a5
o-shmidke <61247061+o-shmidke@users.noreply.github.com>
2020-08-19 23:13:56 +0500

    !

--- CMW/settings/production.py
+++ CMW/settings/production.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'work',
     'materials',
     'import_export',
-'storages',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -153,7 +153,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_URL = os.environ.get('AWS_URL')
-
+AWS_REGION = os.environ.get('AWS_REGION')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_MEDIA_URL = "{}/{}/".format(AWS_URL, AWS_STORAGE_BUCKET_NAME)
