d09e0010f1358823b6d11ae95308a23ac9e1ed5b
o-shmidke <61247061+o-shmidke@users.noreply.github.com>
2020-08-20 17:37:38 +0500

    !

--- project/models.py
+++ project/models.py
@@ -51,6 +51,8 @@ class CustomUser(AbstractUser):
         self.img.delete()
         # super().delete(*args, **kwargs)
 
+
+
     def __str__(self):
         return '%s %s %s' % (self.last_name, self.first_name, self.patronymic_name)
 
