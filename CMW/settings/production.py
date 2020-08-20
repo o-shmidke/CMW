0e8b23379a725c19bc4b36ba336682998e658219
o-shmidke <61247061+o-shmidke@users.noreply.github.com>
2020-08-20 16:22:07 +0500

    !

--- CMW/settings/production.py
+++ CMW/settings/production.py
@@ -157,7 +157,7 @@ AWS_S3_BUCKET_NAME = os.environ.get('AWS_S3_BUCKET_NAME')
 AWS_URL = os.environ.get('AWS_URL')
 AWS_REGION = os.environ.get('AWS_REGION')
 AWS_S3_SIGNATURE_VERSION = 's3v4'
-# AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
